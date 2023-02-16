01f2d0997d79047439f8f59739167556aa468969
Rwedoll <ragn.ryzen@gmail.com>
2023-02-16 12:43:50 +0000

    Pequenas correcções de estilos e icons

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { SiBookstack } from "react-icons/si";
-import { HiAcademicCap } from "react-icons/hi";
+import { ImBook } from "react-icons/im";
+import { VscChecklist } from "react-icons/vsc";
 import { AiFillCaretRight } from "react-icons/ai";
 import Card from "../components/Cards/Card";
 import SectionHead from "./SectionHead";
@@ -10,28 +10,28 @@ const Services = () => {
   const [services] = useState([
     {
       id: 1,
-      icon: <SiBookstack />,
+      icon: <ImBook />,
       title: "Primeiro Ciclo",
       info: "Acompanhamento na primeira etapa escolar até ao quarto ano",
       path: "/offers",
     },
     {
       id: 2,
-      icon: <SiBookstack />,
+      icon: <ImBook />,
       title: "Segundo e Terceiro Ciclo",
       info: "Todo o percurso e aconselhamento para a definição do Secundário ",
       path: "/offers",
     },
     {
       id: 3,
-      icon: <SiBookstack />,
+      icon: <ImBook />,
       title: "Ensino Secundário",
       info: "Acompanhamento e preparação para a vida académica",
       path: "/offers",
     },
     {
       id: 4,
-      icon: <SiBookstack />,
+      icon: <ImBook />,
       title: "Programa RVCC",
       info: "Reconhecimento, Validação e Certificação de competências adquiridas",
       path: "/offers",
@@ -41,7 +41,7 @@ const Services = () => {
   return (
     <section className="services">
       <div className="container services__container">
-        <SectionHead icon={<HiAcademicCap />} title="Serviços" />
+        <SectionHead icon={<VscChecklist />} title="Serviços" />
         <div className="services__wrapper">
           {services.map(({ id, icon, title, info, path }) => {
             return (
